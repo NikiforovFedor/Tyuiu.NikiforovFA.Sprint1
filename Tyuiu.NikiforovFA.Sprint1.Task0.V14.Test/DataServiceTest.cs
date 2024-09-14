7d0db6c0813c8c439e717ee6ceaf7c2e2941d4a2
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-09-14 19:56:34 +0500

    Add project files.

---
+++ Tyuiu.NikiforovFA.Sprint1.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.NikiforovFA.Sprint1.Task0.V14.Lib;
+namespace Tyuiu.NikiforovFA.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ Tyuiu.NikiforovFA.Sprint1.Task0.V14/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.NikiforovFA.Sprint1.Task0.V14.Lib;
+
+namespace Tyuiu.NikiforovFA.Sprint1.Task0.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #0 | Выполнил: Никифоров Ф. А | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Никифоров Федор Александрович | ПКТб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 2*3*3+7 и печатает      *");
+            Console.WriteLine("* результат на экране.                                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2*3*3+7                                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
