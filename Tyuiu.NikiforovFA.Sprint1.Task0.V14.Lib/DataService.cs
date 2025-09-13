456fe9685424cdb04d45712680c30ab7d2866251
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2025-09-13 20:28:54 +0500

    Test

--- Tyuiu.NikiforovFA.Sprint1.Task0.V14.Lib/DataService.cs
+++ Tyuiu.NikiforovFA.Sprint1.Task0.V14.Lib/DataService.cs
@@ -3,7 +3,7 @@ namespace Tyuiu.NikiforovFA.Sprint1.Task0.V14.Lib
 {
     public class DataService : ISprint1Task0V14
     {
-        public double Calculate1()
+        public double Calculate()
         {
             return 2 * 3 * 3 + 7;
         }
